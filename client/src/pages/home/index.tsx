7d4c3c76ce4165ad0cc76fafdfdc9909277375ab
Panos Koutsoukos <pankouts@bu.edu>
2024-12-03 17:30:34 -0500

    Added logo in the home page

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -3,16 +3,27 @@ import styles from "@/styles/Home.module.scss";
 import { withAuth } from "@/utils/auth";
 import type { NextPage } from 'next';
 import Navbar from "../layout/navbar";
+import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
+    <>
     <div className={styles.container}>
       <Navbar />
       <main className={styles.main}>
         <h1>Welcome to Spark! Bytes</h1>
         <p>Discover free food and events happening on the BU campus.</p>
       </main>
+      <div className={styles.logoContainer}>
+            <Image
+              src="/spark.png" // The path to your logo in the public folder
+              alt="Spark Bytes Logo"
+              width={400} // Adjust the width
+              height={400} // Adjust the height
+              />
+            </div>
     </div>
+    </>
   );
 }
 
