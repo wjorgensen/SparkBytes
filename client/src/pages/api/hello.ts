7d821209d55c03defcaf37b65421c770eab50665
AbdullahTAlajmi <alajmi@bu.edu>
2024-12-08 15:38:41 -0500

    api documentation

--- client/src/pages/api/hello.ts
+++ client/src/pages/api/hello.ts
@@ -5,6 +5,14 @@ type Data = {
   name: string;
 };
 
+/**
+ * API route handler for the hello endpoint.
+ * 
+ * @param req - The incoming request object, containing request data.
+ * @param res - The response object used to send data back to the client.
+ * 
+ * This handler responds with a JSON object containing a name.
+ */
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
